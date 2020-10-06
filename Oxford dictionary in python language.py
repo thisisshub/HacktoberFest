760e33e91312e7cd6eeb4b59c9db30df2625acc3
unknown <anujmishraan2005@gmail.com>
2020-10-06 19:10:28 +0530

    Oxford dictionary

---
+++ Oxford dictionary in python language.py
@@ -0,0 +1,9 @@
+Python 3.3.3 (v3.3.3:c3896275c0f6, Nov 18 2013, 21:19:30) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> # create a dictionary
+>>> dic1={'Fruit':'Apple','Climate':'Cold','Price(kg)':120}
+>>> print(dic1)
+{'Price(kg)': 120, 'Climate': 'Cold', 'Fruit': 'Apple'}
+>>> print(dic1['Price(kg)'])
+120
+>>> 
