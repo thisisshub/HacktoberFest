62b34c6a81911435084209c7555a7c63d5ca561d
priyarajoria <72511268+priyarajoria@users.noreply.github.com>
2020-10-07 20:53:21 +0530

    arthimetic-calculator.py

---
+++ arthimetic-calculator.py
@@ -0,0 +1,7 @@
+x = int(input("What is the first number? "))
+y = int(input("What is the second number? "))
+ 
+print("The sum is: ", x+y)
+print("The difference is: ", x-y)
+print("The product is: ", x*y)
+print("The quotient is: ", x//y)
