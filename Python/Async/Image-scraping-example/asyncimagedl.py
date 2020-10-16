d8d44858851148e42e03d9ccf4876aed50a2da69
ayushk780 <40500298+ayushk780@users.noreply.github.com>
2020-10-16 20:30:36 +0530

    Create asyncimagedl.py

---
+++ Python/Async/Image-scraping-example/asyncimagedl.py
@@ -0,0 +1,27 @@
+# Example Python script to scrape images from web
+
+import random
+import io
+import aiohttp
+import requests
+import os
+import urllib3
+import asyncio
+from PIL import Image
+
+
+async def async_fetch(url: str) -> bytes:
+  """fetch html content of  a page. Returns a bytestring of the content"""
+  async with aiohttp.ClientSession() as session:
+      async with session.get(url) as response:
+          html_in_bytestring = await response.content.read()
+          return html_in_bytestring
+
+async def waifu_fun():
+  rnum = random.randint(0, 100000)
+  url = 'https://www.thiswaifudoesnotexist.net/example-' + str(rnum) + '.jpg'
+  html = await async_fetch(url)
+  img = Image.open(io.BytesIO(html))
+  img.save(f'{rnum}.jpg')
+  print("Image Saved")
+asyncio.run(waifu_fun())
