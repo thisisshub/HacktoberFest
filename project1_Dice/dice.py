9ac87ad248377124837d6256306eade76dc517b0
manasisaiba <manasisaiba99@gmail.com>
2020-10-18 12:35:09 +0530

    #HACKTOBERFEST

---
+++ project1_Dice/dice.py
@@ -0,0 +1,42 @@
+import random
+x="y"
+while x=="y":
+    number=random.randint(1,6)
+
+    if number == 1:
+        print(" -------- ")
+        print("|        |")
+        print("|    0   |")
+        print("|        |")
+        print(" -------- ")
+    if number == 2:
+        print(" -------- ")
+        print("|        |")
+        print("| 0    0 |")
+        print("|        |")
+        print(" -------- ")
+    if number == 3:
+        print(" -------- ")
+        print("|    0   |")
+        print("|    0   |")
+        print("|    0   |")
+        print(" -------- ")
+    if number == 4:
+        print(" -------- ")
+        print("| 0    0 |")
+        print("|        |")
+        print("| 0    0 |")
+        print(" -------- ")
+    if number == 5:
+        print(" -------- ")
+        print("| 0    0 |")
+        print("|    0   |")
+        print("| 0    0 |")
+        print(" -------- ")
+    if number == 6:
+        print(" -------- ")
+        print("|  0   0 |")
+        print("|  0   0 |")
+        print("|  0   0 |")
+        print(" -------- ")
+    x=input("do u want to throw again press y if not then n")
