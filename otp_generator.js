4123df00ad57a462abe2f7543ab9ac1c6c0bdaa4
kashyap.sojitra <kashyap.sojitra@byteprophecy.com>
2020-10-20 02:40:26 +0530

    added a function to generate otp of desired length

---
+++ otp_generator.js
@@ -0,0 +1,23 @@
+/*
+The snippet generates OTP of supplied length. If no length is supplied it generates a 6 digit OTP.
+
+- We initiate an empty string for otp.
+- We loop from 0 till length of the otp and keep attaching random numbers to the OTP using Math.random() and Math.floor().
+*/
+
+const otpGenerator = (otpLength) => {
+  const length = otpLength ? otpLength : 6;
+  let otp = "";
+  for (let i = 0; i < length; i++) {
+    otp += Math.floor(Math.random() * 10);
+  }
+  return otp;
+};
+
+/* 
+-------SAMPLE OUTPUT--------
+otpGenerator(4); // "9954"
+otpGenerator(); // "649880"
+otpGenerator(5); // "82139"
+
+*/
