3ae5c70bf4ba8f88e06bfe7466260d8c67ffe738
Aemonjabbar <49372579+Aemonjabbar@users.noreply.github.com>
2020-10-19 18:49:12 +0500

    Update ReverseArray.java

--- Java/ReverseArray.java
+++ Java/ReverseArray.java
@@ -33,3 +33,21 @@ class Q15{
         System.out.println("\n");
     }
 }
+
+class ArrayStarsReverse
+{
+	public static void main(String args[])
+	{
+	// char matrix[][]={{'*'},{'*','*'},{'*','*','*'}};
+		char matrix[][]= new char[][]{new char []{'*'} , new  char[]{'*', '*'} , new char[]{'*' , '*' , '*'}};
+	for(int i=matrix.length-1;i>=0;i--)
+	{
+	  for(int j=0;j<matrix[i].length;j++)
+	  {
+
+	  System.out.print(matrix[i][j]);
+	  }
+	
+	System.out.println();
+	}}
+}
