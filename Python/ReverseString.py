a4d496ea2625defdcd933df9b68fb254c4e7e52e
Ghulam Mohiyuddin <64365552+ghulam98@users.noreply.github.com>
2020-10-20 11:22:02 +0530

    Update ReverseString.py
    more Efficient ways

--- Python/ReverseString.py
+++ Python/ReverseString.py
@@ -1,7 +1,10 @@
+# a='hello;
+# a[::-1] --> 'olleh'
+
 def RevS(a):
 	a=str(a)
-	for i in range(len(a)-1,-1,-1):
-		print(a[i],end="")
+	for i in a[::-1]: # efficient way in python
+		print(i,end="")
 	print()
 s=input()
 RevS(s)
