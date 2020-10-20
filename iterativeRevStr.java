e2a1d0182e37ebfa2f6f023b77c1ed54766836e3
dgitsme <33507734+dgitsme@users.noreply.github.com>
2020-10-20 16:02:57 +0530

    iterativeRevStr.java

---
+++ iterativeRevStr.java
@@ -0,0 +1,11 @@
+public class FinalReverseWithoutUsingStringMethods {
+  
+     public static void main(String[] args) {
+          String str = "Automation";
+          StringBuilder str2 = new StringBuilder();
+          str2.append(str);
+          str2 = str2.reverse();     // used string builder to reverse
+         System.out.println(str2);
+          }
+  
+}
