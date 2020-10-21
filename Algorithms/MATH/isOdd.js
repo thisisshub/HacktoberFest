884a2fd1dd0d19009e92d5e51192f8f839835ddb
theepag <theepag@live.com>
2020-10-21 14:37:06 +0530

    Added isDivisible and isOdd javascript algorithm

---
+++ Algorithms/MATH/isOdd.js
@@ -0,0 +1,13 @@
+/*
+ * function that checkd if a number is odd
+ * return true if number is odd
+ * else false
+ */
+
+const isOdd = (value) => {
+  return !!((value & 1))
+}
+
+// testing
+console.log(isOdd(2))
+console.log(isOdd(3))
