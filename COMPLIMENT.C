05f7592bae02a3261455ff155c887d5745e12534
Utkarsh016 <33507776+Utkarsh016@users.noreply.github.com>
2020-10-22 12:35:59 +0530

    COMPLIMENT.C
    COMPLIMENT.C

---
+++ COMPLIMENT.C
@@ -0,0 +1,12 @@
+// C program to print maximum value of 
+// unsigned int. 
+#include<stdio.h> 
+
+int main() 
+{ 
+unsigned int max; 
+max = 0; 
+max = ~max; 
+printf("Max value : %u ", max); 
+return 0; 
+}	 
