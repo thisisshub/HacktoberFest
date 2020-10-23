909fc3f8ecf55f151fdd215b62443da088270457
ankitkumar5358 <57992836+ankitkumar5358@users.noreply.github.com>
2020-10-23 15:31:07 +0530

    A 2D Array Program is Added

---
+++ 2darray.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int
+main()
+{
+	int arr[5][5];//program of 2-D Array
+	for(int i=0;i<5;i++)
+	{
+		for(int j=0;j<5;j++)
+		{
+		 cin>>arr[i][i];
+		}
+		cout<<endl;
+	}
+	for(int i=0;i<5;i++)
+	{
+		for(int j=0;j<5;j++)
+		{
+			cout<<arr[i][i];
+		}
+		cout<<endl;
+	}
+	
+
+	return 0;
+}
