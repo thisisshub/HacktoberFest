0a95f3ba0ffc8680eb5c09438c400c73146e7e5b
blaze127 <44069915+VGJeyasurya@users.noreply.github.com>
2020-10-23 12:38:28 +0530

    Update mac changer.py

--- mac changer.py
+++ mac changer.py
@@ -1,5 +1,22 @@
-import subprocess
+#!/usr/bin/env python
  
-subprocess.call(["sudo","ifconfig","ens33","down"])
-subprocess.call(["sudo","ifconfig","ens33","hw","ether","00:11:22:33:44:55"])
-subprocess.call(["sudo","ifconfig","ens33","up"])
+import subprocess
+import optparse
+ 
+ 
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    return parser.parse_args()
+ 
+ 
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+ 
+ 
+(options, arguments) = get_arguments()
+change_mac(options.interface, options.new_mac)
