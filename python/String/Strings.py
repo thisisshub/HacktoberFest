ef30443ee7919c134cac6454edca00674fe48bfe
singhsagarperfect <73217974+singhsagarperfect@users.noreply.github.com>
2020-10-24 21:07:07 +0530

    Update Strings.py

--- python/String/Strings.py
+++ python/String/Strings.py
@@ -1,14 +1,6 @@
-def checksub(T, P):
-    for i in range(len(T)-len(P)+1):
-        b = T[i:len(P)+i]
-        if P == b:
-            return True
-    return False
-
-
 a = "abcdefghijklmnopqrstuvwxyz"
 b = "mnopi"
-print(checksub(a, b))
+print(b in a)
 
 # T(n)=O(len(P),len(T))
 # This is a naive algorithm
