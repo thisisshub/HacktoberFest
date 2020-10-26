8ff6b998f0b294900ac8e2fb2f1423c65f4e39ac
AyushBilkhiwal <69378143+AyushBilkhiwal@users.noreply.github.com>
2020-10-27 00:08:37 +0530

    Update Day1.py

--- python/30 days of code/Day1.py
+++ python/30 days of code/Day1.py
@@ -1,8 +1,9 @@
-i = 4
+i = 4 
 d = 4.0
 s = 'HackerRank '
-a=int(input())
-b=float(input())
+a=int(input('Enter the integer value of a: '))
+b=float(input('Enter the float value of b: '))
+#please enter the string as the input of c
 c=input()
 print(a+i)
 print(b+d)
