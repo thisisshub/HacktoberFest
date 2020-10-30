7b867cbb9680fca73fe579ccb717126ac5e85d92
Kratika Gupta <54476432+kratikagupta2002@users.noreply.github.com>
2020-10-30 14:18:13 +0530

    included power function

--- cpp/Calculator_C++.cpp
+++ cpp/Calculator_C++.cpp
@@ -2,36 +2,39 @@
 *, /). The program should take two operands from user and perform the
 operation on those two operands depending upon the operator entered by
 user. */
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
 	cout<<"\nWelcome to Calculator.\n Please enter data in the following format:-\n"; 
-	cout<<"(number1)(space)(operator)(space)(number2) \n";
-	float number1,number2,number3;
+	cout<<"(num1)(space)(operator)(space)(num2) \n";
+	float num1,num2,num3;
 	char op,a;	
 	do{
-		cin>>number1>>op>>number2;
+		cin>>num1>>op>>num2;
 		switch(op){
 			case '+':
-				number3=number1+number2;
+				num3=num1+num2;
 				break;
 			case '-':
-				number3=number1-number2;
+				num3=num1-num2;
 				break;
 			case '*':
-				number3=number1*number2;
+				num3=num1*num2;
 				break;
 			case '/':
-				if(number2==0)
+				if(num2==0)
 				{cout<<"Numbers invalid for operation, please try again.";
 				break;}
 				else
-				{number3=number1/number2;
+				{numr3=num1/num2;
 				break;}
+			case '^':
+				num3=pow(num1,num2);
+				break;
 			default:
 				cout<<"Operator not recognised. Please try again.";
 			}
-		cout<<"Answer: \t"<<number3<<endl;
+		cout<<"Answer: \t"<<numb3<<endl;
 		cout<<"Do you want to try again?(y/n)\n";
 		cin>>a;
 	}while(a=='y');
