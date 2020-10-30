ca65e34f6011c4c09c974bf3dedf806aff3c9f3c
Diego Vieira <divieira.silva@gmail.com>
2020-10-30 17:41:43 -0300

    Hacktober First Commit

---
+++ net/hacktober-first/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hacktober_first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Can't belive this worked! :O");
+        }
+    }
+}
