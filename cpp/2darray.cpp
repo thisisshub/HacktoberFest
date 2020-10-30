8bda541bceeb69c7f43c26d1bea829796cdf6eeb
Kratika Gupta <54476432+kratikagupta2002@users.noreply.github.com>
2020-10-30 14:21:44 +0530

    Updated 2-D array

--- cpp/2darray.cpp
+++ cpp/2darray.cpp
@@ -1,10 +1,8 @@
-#include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-int
-main()
+int main()
 {
-	int arr[5][5];//program of 2-D Array
+	int arr[5][5];//creates a 2-D Array
 	for(int i=0;i<5;i++)
 	{
 		for(int j=0;j<5;j++)
