4f310a774e2c48b95759d73408f93e3339b3583b
ssahibsingh <75523820+ssahibsingh@users.noreply.github.com>
2021-10-01 19:44:02 +0530

    Update pattern.java

--- java/pattern.java
+++ java/pattern.java
@@ -1,26 +1,35 @@
-package com.practice;
-
-public class buubleSort {
-    public static void main(String[] args) {
-
-        System.out.println("Increasing star pattern ");
-        for (int i = 1; i <= 5; i++ ) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-
-        System.out.println();
-
-        System.out.println("Decreasing star pattern ");
-        for (int i = 1; i <= 5; i++ ) {
-            for (int j = i; j < 5; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-
-
-    }
-}
+public class pattern{  
+    public static void main(String []args){  
+      
+       int lines=8;  
+        int i,j;  
+        for(i=1;i<lines;i++){// this loop is used to print the lines  
+            for(j=1;j<=lines/2;j++){// this loop is used to print numbers  
+                if(i==j){  
+                    System.out.print(j);  
+                }  
+                else if(i>4 && j==lines-i){  
+                    System.out.print(j);  
+                }  
+                else{  
+                   System.out.print(" ");  
+                }  
+            }  
+            j=j-2;  
+            while(j>0){ //this loop is used to print numbers  
+                if(i==j){  
+                System.out.print(j);  
+                }  
+                else if(i>4 && j==lines-i){  
+                    System.out.print(j);  
+                }  
+                else{  
+                    System.out.print(" ");  
+                }  
+      
+                j--;  
+            }  
+            System.out.println("");  
+        }  
+         }  
+    }  
