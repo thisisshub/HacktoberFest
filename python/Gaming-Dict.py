db5999c4b9218287318d754973c2d1ebd416417c
Ragnarok1023 <72254963+Ragnarok1023@users.noreply.github.com>
2021-10-01 19:20:32 +0530

    Create Gaming-Dict.py

---
+++ python/Gaming-Dict.py
@@ -0,0 +1,10 @@
+"""
+Dictionary of PC Games
+"""
+
+d = {"MAX":"PAYNE", "STAR":"WARS", "CONTROL":1, "GTA":5, "FARCRY":3, "WITCHER":"GERALT OF RIVIA"}
+print("Enter a word")
+w = input().upper()
+# print(len(d))
+print(w,d[w])
+# print(d[w].lower().capitalize())
