4e6043b7f7baebaa58c48067b48d23cc5ca33e3d
Shuvo <38564994+0shuvo0@users.noreply.github.com>
2021-10-02 09:31:56 +0600

    Create debounce.js

---
+++ javascript/debounce.js
@@ -0,0 +1,7 @@
+function debounce(func, timeout = 300){
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { func.apply(this, args); }, timeout);
+  };
+}
