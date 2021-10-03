101616407ea711d1cbc2e6c8c44ddef8b05997d3
rishusiva <rishusiva@gmail.com>
2021-10-03 18:00:01 +0530

    Update Palindrome.java

--- Java/Palindrome.java
+++ Java/Palindrome.java
@@ -1,40 +1,28 @@
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-public class Palindrome 
-{
-    public static void main (String[] args) {
-        Scanner sc= new Scanner(System.in);
-        
-        System.out.println("Enter the word :");
-        String str=sc.nextLine();
-        int len=str.length();
-        
-        Pattern p=Pattern.compile("[^a-z0-9]", Pattern.CASE_INSENSITIVE);
-        Matcher m=p.matcher(str);
-        boolean b=m.find();
-       
-        if(b)
-        {
-            System.out.println("Invalid Input");
-        }
-        else
-        {
-            String rvs=new String();
-            
-            for(int i=(len-1); i>=0; i--)
-            {
-                rvs=rvs+str.charAt(i);
-            }
-            
-            if(str.equalsIgnoreCase(rvs))
-            {
-                System.out.println(str+" is a Palindrome");
-            }
-            else
-            {
-                System.out.println(str+" is not a Palindrome");
-            }
-        }
-    }
+//Java Program to check whether a number is palindrome or not
+
+import java.util.Scanner;
+
+public class Palindrome {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter A Number :");
+		int number = sc.nextInt();
+		
+		int org,res=0,rem;
+		
+		org=number;
+		while(org!=0) {
+			rem=org%10;
+			res=(res*10)+rem;
+			org/=10;
+		}
+		System.out.println(res);
+		
+		if(number==res) {
+			System.out.println(number+" is a Palindrome Number");
+		}
+		else {
+			System.out.println(number+" is not Palindrome Number");
+		}
+	}
 }
