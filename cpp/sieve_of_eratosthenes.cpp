6d23991ec905bb46f6222ec745de9103876b0a5c
Lakshya Garg <garg.lakshya@gmail.com>
2021-10-03 17:39:37 +0530

    Added sieve_of_eratosthenes.cpp

---
+++ cpp/sieve_of_eratosthenes.cpp
@@ -0,0 +1,32 @@
+// Sieve Of Eratosthenes
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void SieveOfEratosthenes(int n) 
+{ 
+    bool prime[n+1]; 
+    memset(prime, true, sizeof(prime)); 
+
+    for (int p=2; p*p<=n; p++) 
+    { 
+        if (prime[p] == true) 
+        { 
+            for (int i=p*p; i<=n; i += p) 
+                prime[i] = false; 
+        } 
+    } 
+
+    for (int p=2; p<=n; p++) 
+       if (prime[p]) 
+          cout << p <<endl; 
+}
+
+int main() {
+   int num;
+   cout << "Enter the number :";
+   cin >> num;
+   SieveOfEratosthenes(num);
+   return 0;
+}
