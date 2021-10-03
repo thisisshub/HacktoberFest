bbf5eb110f59ce146a7b9ba50978d83d34fe59a7
Ruben Falcao <79042516+RubenFalcao14@users.noreply.github.com>
2021-10-03 20:20:26 +0530

    Add files via upload

--- Java/Is_Prime.java
+++ Java/Is_Prime.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Prime {
+public class Is_Prime {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
