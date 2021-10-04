bf0fee0e9c32c6a46d2c86c94b55d4b6da33a2b5
thisisshub <thisisshub@gmail.com>
2021-10-04 12:35:33 +0530

    cleanup

--- misc/2SUM.rb
+++
@@ -1,4 +0,0 @@
-A = gets.to_i
-B = gets.to_i
-
-puts "X = #{A + B}"
--- misc/java/Primitive-type-conversions-Codefiles/Code/TypeConversion.java
+++
@@ -1,8 +0,0 @@
-public class TypeConversion {
-	public static void main(String[] args) {
-		String sHeight = "65.43";
-		
-		float h = Float.parseFloat(sHeight);
-		System.out.println(h);
-	}
-}
